9e14c017f55861468d3d8e292606f00627a4354b
Joelokechu <100615808+Joelokechu@users.noreply.github.com>
2026-09-10 20:16:21 +0100

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+window.HELEN_ESTATES_CONFIG = {
+  supabaseUrl: 'https://mzjirlnenllftyyzjuti.supabase.co',
+  supabasePublicKey: 'sb_publishable_wasn-KNaoQoqOVP5TeEQ_w_Ui7x62LN',
+  siteUrl: 'https://helenestatesrealtors.com'
+};
